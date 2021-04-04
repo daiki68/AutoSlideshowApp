8c213206a447c24a5c17f9cf4bde0d561bd8af5c
daiki.abe <h12293812s@gmail.com>
2021-04-04 15:20:37 +0900

    課題「スライドショーアプリ」

---
+++ app/src/main/java/jp/techacademy/daiki/autoslideshowapp/MainActivity.kt
@@ -0,0 +1,134 @@
+package jp.techacademy.daiki.autoslideshowapp
+
+import android.Manifest
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.pm.PackageManager
+import android.os.Build
+import android.os.Handler
+import android.util.Log
+import android.provider.MediaStore
+import android.content.ContentUris
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val PERMISSIONS_REQUEST_CODE = 100
+
+    private var mTimer: Timer? = null
+    private var mTimerSec = 0.0
+    private var mHandler = Handler()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+                getContentsInfo()
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE), PERMISSIONS_REQUEST_CODE)
+            }
+        } else {
+            getContentsInfo()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSIONS_REQUEST_CODE ->
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getContentsInfo()
+                } else {
+                    move_button.isClickable = false
+                    return_button.isClickable = false
+                    play_button.isClickable = false
+                }
+        }
+    }
+
+    private fun getContentsInfo() {
+        val resolver = contentResolver
+        val cursor = resolver.query(
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+            null,
+            null,
+            null,
+            null
+        )
+
+        move_button.setOnClickListener {
+
+            if (cursor!!.moveToNext()) {
+                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor.getLong(fieldIndex)
+                val imageUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                imageView.setImageURI(imageUri)
+
+            } else (cursor.moveToFirst())
+        }
+
+        return_button.setOnClickListener {
+
+            if (cursor!!.moveToPrevious()) {
+                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                val id = cursor.getLong(fieldIndex)
+                val imageUri =
+                    ContentUris.withAppendedId(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, id)
+
+                imageView.setImageURI(imageUri)
+
+            } else (cursor.moveToLast())
+        }
+
+        play_button.setOnClickListener {
+            if (mTimer == null){
+                mTimer = Timer()
+                mTimer!!.schedule(object : TimerTask() {
+                    override fun run() {
+                        mTimerSec += 2
+                        mHandler.post {
+                            if (cursor!!.moveToNext()) {
+                                val fieldIndex = cursor.getColumnIndex(MediaStore.Images.Media._ID)
+                                val id = cursor.getLong(fieldIndex)
+                                val imageUri =
+                                    ContentUris.withAppendedId(
+                                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                                        id
+                                    )
+
+                                imageView.setImageURI(imageUri)
+
+                            }
+                        }
+                    }
+                }, 2000, 2000)
+
+                play_button.text = "停止"
+
+                move_button.isClickable = false
+                return_button.isClickable = false
+
+            } else {
+                if (mTimer != null) {
+                    mTimer!!.cancel()
+                    mTimer = null
+                }
+
+                play_button.text = "再生"
+
+                move_button.isClickable = true
+                return_button.isClickable = true
+
+            }
+        }
+
+
+
+
+
+    }
+}
